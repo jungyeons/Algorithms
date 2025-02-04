f23e7b1bde5b5584ffc317e8550bc0e3003a1223
BaeJungYeon <qowjdduswkd@naver.com>
2025-02-04 16:39:24 +0900

    Create Main11005.java

---
+++ 1.완전탐색 & 시뮬레이션/Main11005.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+    public class Main11005 {
+
+            public static void main (String[] args) {
+                Scanner sc = new Scanner(System.in);
+                int N = sc.nextInt();
+                int B = sc.nextInt();
+
+                String ans = "";
+                while (N > 0) {
+                    int digit = N % B;
+                    if (digit < 10) ans += digit;
+                    else ans += (char)('A' + digit - 10);
+                    N /= B;
+                }
+
+                System.out.println(new StringBuilder(ans).reverse());
+            }
+        }
+    }
